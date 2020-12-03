5993121bf5388b05979c9e76911d45a8afa60d9d
Gustavo Mendes <gustavoomendesoliveira@gmail.com>
2020-12-03 03:01:11 -0300

    Adicionando CORS

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from resources.EmpresasResource import Bovespa,Empresas
-
+from resources.CompaniesResource import Bovespa,Empresas
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
-        
+
+ 
 api.add_resource(Bovespa,'/')
 api.add_resource(Empresas,'/<string:symbol>')
 
